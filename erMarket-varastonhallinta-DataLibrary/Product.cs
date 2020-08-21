bdaab8a12e16f6c81af49e99c87c07b159e91396
Eetu Rinkinen <eetu.rinkinen@gmail.com>
2020-08-21 19:29:22 +0300

    Upper ui & some classes

--- erMarket-varastonhallinta-DataLibrary/Product.cs
+++ erMarket-varastonhallinta-DataLibrary/Product.cs
@@ -10,6 +10,7 @@ namespace erMarket_varastonhallinta_DataLibrary
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Price { get; set; }
         public int InStock { get; set; }
 
     }
---
+++ erMarket-varastonhallinta-DataLibrary/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erMarket_varastonhallinta_DataLibrary
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Supervisor { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Store()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
