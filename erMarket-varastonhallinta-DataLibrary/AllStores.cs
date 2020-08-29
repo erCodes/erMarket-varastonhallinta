a0b02821380d719925d968df699c7f2e24832a9c
Eetu Rinkinen <eetu.rinkinen@gmail.com>
2020-08-29 03:10:09 +0300

    New UI components & Dto:s

---
+++ erMarket-varastonhallinta-DataLibrary/AllStores.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erMarket_varastonhallinta_DataLibrary
+{
+    public class AllStores
+    {
+        // This file is temporary and it will be replaced as the work progresses.
+        public static List<Store> GetStores()
+        {
+            List<Store> stores = new List<Store>();
+
+            stores.Add(new Store {
+            Id = 1,
+            City = "Helsinki",
+            Address = "Simonkatu 14",
+            Supervisor = "-",
+            Phone = "000000001",
+            Email = "hlki@ermarket.fi"
+            });
+
+            stores.Add(new Store
+            {
+                Id = 2,
+                City = "Tampere",
+                Address = "Rautatienkatu 10",
+                Supervisor = "-",
+                Phone = "000000002",
+                Email = "tmpr@ermarket.fi"
+            });
+
+            stores.Add(new Store
+            {
+                Id = 3,
+                City = "Jyväskylä",
+                Address = "Kauppakatu 24",
+                Supervisor = "-",
+                Phone = "000000003",
+                Email = "jkl@ermarket.fi"
+            });
+
+            stores.Add(new Store
+            {
+                Id = 4,
+                City = "Oulu",
+                Address = "Saaristonkatu 8",
+                Supervisor = "-",
+                Phone = "000000004",
+                Email = "oulu@ermarket.fi"
+            });
+
+            return stores;
+        }
+    }
+}
--- erMarket-varastonhallinta-DataLibrary/Product.cs
+++ erMarket-varastonhallinta-DataLibrary/Product.cs
@@ -11,7 +11,30 @@ namespace erMarket_varastonhallinta_DataLibrary
         public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
+        public List<Categories> Type { get; set; }
         public int InStock { get; set; }
+        public DateTime QuantityChanged { get; set; }
 
+        public Product()
+        {
+            Type = new List<Categories>();
+        }
+    }
+
+    public enum Categories
+    {
+        Groceries,
+        Vegetables,
+        Fruits,
+        MeatProducts,
+        FishProducts,
+        DairyProducts,
+        ReadyMeals,
+        Drinks,
+        Preserves,
+        FrozenFoods,
+        Tools,
+        CleaningProducts,
+        DentalCareProducts
     }
 }
